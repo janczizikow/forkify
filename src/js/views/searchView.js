985d2786c112586744d7cafbb09ed85511af401a
Jan Czizikow <jan.czizikow@gmail.com>
2018-05-17 12:19:32 +0800

    Fixing bug with error on removing hilighted searchlist if the id is not there

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -12,11 +12,13 @@ export const clearResults = () => {
 };
 
 export const highlightSelected = id => {
-  const resultsArr = Array.from(document.querySelectorAll('.results__link'));
-  resultsArr.forEach(el => {
-    el.classList.remove('results__link--active');
-  });
-  document.querySelector(`.results__link[href="#${id}"]`).classList.add('results__link--active');
+  if ( document.querySelector(`.results__link[href="#${id}"]`) ) {
+    const resultsArr = Array.from(document.querySelectorAll('.results__link'));
+    resultsArr.forEach(el => {
+      el.classList.remove('results__link--active');
+    });
+    document.querySelector(`.results__link[href="#${id}"]`).classList.add('results__link--active');
+  }
 }
 
 
